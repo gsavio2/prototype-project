e80ffed290f9b1837db12fece33726123b694841
Pedro Henrique Machado Silva <pedro.machado@castgroup.com.br>
2019-03-18 15:24:41 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="nav">
+      <Header></Header>
+    </div>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from '@/components/layout/Header.vue'
 
 export default {
-  name: 'app',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
+
 
 <style>
 #app {
